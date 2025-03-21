20abedc4b86aab96e9ae96631716575a5df69711
MattButProfessional <bhammatt21@gmail.com>
2025-03-21 10:28:16 -0400

    DONE :3

--- movie-site/src/pages/Movie.jsx
+++ movie-site/src/pages/Movie.jsx
@@ -30,20 +30,39 @@ function Movie({ movieId }) {
 
   return (
     <div className="Container">
-      <h1>{movie.title}</h1>
+      <h1 className="DetailH1">{movie.title}</h1>
       <div className="Container-Two">
         <img
+          className="DetailImg"
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.title}
         />
         <div className="Info-Container">
-          <h2>Release Date</h2>
-          <p>{movie.release_date?.split("-")[0]}</p>
-          <h2>Overview</h2>
-          <p>{movie.overview}</p>
+          <h2 className="DetailH2">Overview</h2>
+          <p className="DetailParagraph">{movie.overview}</p>
+          <h2 className="DetailH2">Release Date</h2>
+          <p className="DetailParagraph">{movie.release_date?.split("-")[0]}</p>
+          <h2 className="DetailH2">Genres</h2>
+          <ul className="genres">
+            {movie.genres.map((genre) => (
+              <li className="DetailList" key={genre.id}>
+                {genre.name}
+              </li>
+            ))}
+          </ul>
+          <h2 className="DetailH2">Budget</h2>
+          <p className="DetailParagraph">
+            {movie.budget === 0
+              ? "N/A"
+              : new Intl.NumberFormat("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                }).format(movie.budget)}
+          </p>
+          <h2 className="DetailH2">Run Time</h2>
+          <p className="DetailParagraph">{movie.runtime} min</p>
         </div>
       </div>
-      <code>{JSON.stringify(movie, null, 2)}</code>
     </div>
   );
 }
